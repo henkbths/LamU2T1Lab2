f5257faaea03443e5fc4acc9060e82c93725e025
Henk Lam <henkl@nycstudents.net>
2023-09-28 11:25:24 -0400

    done

---
+++ src/CircleRunner.java
@@ -0,0 +1,13 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle = new Circle(5.0);
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle.printArea();
+        // C. BEFORE running this code, PREDICT what gets printed
+        // 78.5
+        // D. run the code and confirm your prediction! Were you correct?
+        // ya
+    }
+}
